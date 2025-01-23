4bd90389666bff72e01cffeab3bfe7fa51b48bc1
adoorknob <quantengwai@gmail.com>
2025-01-23 20:27:29 +0800

    change name

---
+++ src/main/java/Duck.java
@@ -0,0 +1,15 @@
+public class Duck {
+    public static void main(String[] args) {
+        String logo = " _____             _    \n"
+                + "|  __  \\_   _  ___| | __\n"
+                + "| |  | | | | |/ __| |/ /\n"
+                + "| |__| | |_| | (__|   <\n"
+                + "|_____/ \\__,_|\\___|_|\\_\\\n";
+        String divider = "____________________________________________________________";
+        System.out.println("Hello! I'm \n" + logo);
+        System.out.println("What can I do for you?");
+        System.out.println(divider);
+        System.out.println("quack");
+        System.out.println(divider);
+    }
+}
