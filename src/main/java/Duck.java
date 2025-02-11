f4dbdd42f6e79341a932fc70f2e1a57de58edf43
adoorknob <quantengwai@gmail.com>
2025-02-11 11:10:16 +0800

    Refactor Duck for readability and modularity

--- src/main/java/Duck.java
+++ src/main/java/Duck.java
@@ -1,21 +1,49 @@
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.ArrayList;
+import java.util.*;
+import java.util.function.Function;
 
 
 public class Duck {
+    static final String DIVIDER = "____________________________________________________________";
+    static final String LOGO = """
+             _____             _
+            |  __  \\_   _  ___| | __
+            | |  | | | | |/ __| |/ /     _
+            | |__| | |_| | (__|   <    <(.)___
+            |_____/ \\__,_|\\___|_|\\_\\    (____/
+            """;
+
     static int BY_COMMAND_BUFFER = 4;
     static int FROM_COMMAND_BUFFER = 6;
     static int TO_COMMAND_BUFFER = 4;
 
-    static String DIVIDER = "____________________________________________________________";
-    static String LOGO = """
-             _____             _
-            |  __  \\_   _  ___| | __
-            | |  | | | | |/ __| |/ /   _
-            | |__| | |_| | (__|   <  <(.)___
-            |_____/ \\__,_|\\___|_|\\_\\  (____/
-            """;
+    static final String COMMAND_LIST = "list";
+    static final String COMMAND_MARK = "mark";
+    static final String COMMAND_UNMARK = "unmark";
+    static final ArrayList<String> COMMAND_EXIT_LIST = new ArrayList<String>(Arrays.asList(
+            "bye",
+            "exit",
+            "quit"
+    ));
+
+    static final String TASK_NAME_TODO = "todo";
+    static final String TASK_NAME_DEADLINE = "deadline";
+    static final String TASK_NAME_EVENT = "event";
+
+    static final String MESSAGE_WELCOME = "Hello! I'm \n" + LOGO + "\n What can I do for you?";
+    static final String MESSAGE_EXIT = "bye bye :(";
+    static final String MESSAGE_ACKNOWLEDGE_TASK_ADDED = "Got it. I've added this task:";
+    static final String MESSAGE_ACKNOWLEDGE_MARK_COMMAND = "Nice! I've marked this task as done:";
+    static final String MESSAGE_ACKNOWLEDGE_UNMARK_COMMAND = "Ok, I've marked this task as not done yet:";
+
+    static final String ERROR_NO_MATCHING_TYPES = "No matching types, please try again :/";
+    static final String ERROR_INVALID_INPUT = "Invalid input, please try again :0";
+
+    static final Map<String, Function<String, Task>> addTaskCommandMap = Map.ofEntries(
+            Map.entry(TASK_NAME_TODO, Duck::addTodo),
+            Map.entry(TASK_NAME_DEADLINE, Duck::addDeadline),
+            Map.entry(TASK_NAME_EVENT, Duck::addEvent)
+    );
+
     static private final ArrayList<Task> taskList = new ArrayList<>();
     static private int taskCounter = 0;
 
@@ -31,8 +59,7 @@ public class Duck {
     }
 
     private static void printWelcomeMessage() {
-        System.out.println("Hello! I'm \n" + LOGO);
-        System.out.println("What can I do for you?");
+        System.out.println(MESSAGE_WELCOME);
         System.out.println(DIVIDER);
     }
 
@@ -42,72 +69,59 @@ public class Duck {
         System.out.println(DIVIDER);
 
         String command = input.toLowerCase();
-
-        switch (command) {
-        case "bye": {
+        if (isExitCommand(command)) {
             exitProgram();
-            break;
         }
-        case "list": {
+
+        // switch case for scalability
+        switch (command) {
+        case COMMAND_LIST: {
             printList();
             break;
         }
         default: {
-            String[] splitInput = input.split(" ");
-            try {
-                switch (splitInput[0]) {
-                case "mark": {
-                    markAsDone(Integer.parseInt(splitInput[1]));
-                    break;
-                }
-                case "unmark": {
-                    markAsUndone(Integer.parseInt(splitInput[1]));
-                    break;
-                }
-                default: {
-                    addToList(input);
-                }
-                }
-            } catch (Exception e) {
-                System.out.println("Something went wrong :(");
-            }
+            markOrCreateTask(input);
         }
         }
     }
 
-
-    private static void addToList(String input) {
+    private static void markOrCreateTask(String input) {
         String[] splitInput = input.split(" ");
-        String inputWithoutCommand = String.join(" ", Arrays.asList(splitInput).subList(1, splitInput.length));
-        Task newTask;
         try {
-            switch (splitInput[0].toLowerCase()) {
-            case "todo": {
-                newTask = addTodo(inputWithoutCommand);
+            switch (splitInput[0]) {
+            case COMMAND_MARK: {
+                markAsDone(Integer.parseInt(splitInput[1]));
                 break;
             }
-            case "deadline": {
-                newTask = addDeadline(inputWithoutCommand);
-                break;
-            }
-            case "event": {
-                newTask = addEvent(inputWithoutCommand);
+            case COMMAND_UNMARK: {
+                markAsUndone(Integer.parseInt(splitInput[1]));
                 break;
             }
             default: {
-                // no matching types
-                printNoMatchingTypesError();
-                return;
+                addToList(input);
             }
             }
-        } catch (NumberFormatException e) {
-            System.out.println("Invalid input, please try again");
+        } catch (Exception e) {
+            System.out.println(ERROR_INVALID_INPUT);
             System.out.println(DIVIDER);
+        }
+    }
+
+    private static void addToList(String input) {
+        String[] splitInput = input.split(" ");
+        String command = splitInput[0];
+        String inputWithoutCommand = String.join(" ", Arrays.asList(splitInput).subList(1, splitInput.length));
+        Task newTask;
+
+        if (addTaskCommandMap.containsKey(command)) {
+            newTask = addTaskCommandMap.get(command).apply(inputWithoutCommand);
+        } else {
+            printNoMatchingTypesError();
             return;
         }
 
         taskList.add(newTask);
-        System.out.println("Got it. I've added this task: ");
+        System.out.println(MESSAGE_ACKNOWLEDGE_TASK_ADDED);
         newTask.printTask();
         taskCounter++;
         System.out.println("Now you have " + taskCounter + " tasks added to the list.");
@@ -147,7 +161,8 @@ public class Duck {
             return;
         }
         taskList.get(taskId - 1).markAsComplete();
-        System.out.println("Nice! I've marked this task as done:\n  [X] " + taskList.get(taskId - 1).getTitle());
+        System.out.println(MESSAGE_ACKNOWLEDGE_MARK_COMMAND);
+        taskList.get(taskId-1).printTask();
         System.out.println(DIVIDER);
     }
 
@@ -158,7 +173,8 @@ public class Duck {
             return;
         }
         taskList.get(taskId - 1).markAsUncomplete();
-        System.out.println("Ok, I've marked this task as not done yet:\n  [ ] " + taskList.get(taskId - 1).getTitle());
+        System.out.println(MESSAGE_ACKNOWLEDGE_UNMARK_COMMAND);
+        taskList.get(taskId-1).printTask();
         System.out.println(DIVIDER);
     }
 
@@ -171,14 +187,17 @@ public class Duck {
         System.out.println(DIVIDER);
     }
 
-
     private static void printNoMatchingTypesError() {
-        System.out.println("No matching types, please try again :/");
+        System.out.println(ERROR_NO_MATCHING_TYPES);
         System.out.println(DIVIDER);
     }
 
+    private static boolean isExitCommand(String command) {
+        return COMMAND_EXIT_LIST.contains(command);
+    }
+
     private static void exitProgram() {
-        System.out.println("bye bye :(");
+        System.out.println(MESSAGE_EXIT);
         System.out.println(DIVIDER);
         System.exit(0);
     }
